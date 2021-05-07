f5f335f6bf6e14b898f81414713a9a3f97ca2bfe
dausuke <ksye616@gmail.com>
2021-05-08 00:02:35 +0900

    add-coodinate

---
+++ casimas_vue/src/components/coodinateImg.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class=" col-12 my-2 p-0 d-flex justify-content-center align-items-center">
+        <div class="m-0 p-3 w-50 item-area__content__img item-img">
+            <img :src="url + itemContents.coodinate_img" class="item m-0" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
--- casimas_vue/src/views/after_login/coodinate.vue
+++ casimas_vue/src/views/after_login/coodinate.vue
@@ -1,5 +1,23 @@
 <template>
-  
+    <div>
+        <mainHeaderBack></mainHeaderBack>
+        <main class="container-fulid user-content">
+            <div class="user-content__home area user-content ">
+                <h2 class="pt-3">コーディネート一覧</h2>
+                <div class="row w-100 m-0">
+                    <div class="item-area w-100 m-0 p-0">
+                        <ul class="row m-0 p-0 w-100">
+                            <li class="col-12 mb-3" v-for="(content, index) in item" :key="index">
+                                <itemImg :itemContents="content" @itemPage="itemPage"></itemImg>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <button v-if="data.sellerid" class="btn btn-dark p-0 sell-btn" type="button"><router-link to="/sell">出品する</router-link></button>
+        </main>
+        <footerMenu></footerMenu>
+    </div>
 </template>
 
 <script>
@@ -8,6 +26,6 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
